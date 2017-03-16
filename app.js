3021718fb97a501668e6053c847cf2a358b2a39d
Aleksey Belchenkov <u608110@gmail.com>
2017-03-16 18:32:46 +0300

    Deleting Task

--- app.js
+++ app.js
@@ -52,6 +52,25 @@ app.post('/todo/add', function(req, res, next) {
     });
 });
 
+app.post('/todo/delete', function(req, res, next) {
+    var delTodos = req.body.todos;
+
+    client.lrange('todos', 0, -1, function(err, todos) {
+        for (var i = 0; i < todos.length; i++) {
+            if (delTodos.indexOf(todos[i]) > -1) {
+                client.lrem('todos', 0, todos[i], function() {
+                    if (err) {
+                        console.log();
+                    }
+                });
+            }
+        }
+
+        res.redirect('/');
+    });
+});
+
+
 app.listen(3000, function() {
     console.log('Server starded on localhost:3000');
 });
